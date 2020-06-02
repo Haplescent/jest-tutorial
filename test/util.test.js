6246d6051b4b8655bcb123e1705c30b2be1b8b06
John Merritt <johnmerritt95@gmail.com>
2020-06-01 19:43:36 -0700

    added failure test

--- test/util.test.js
+++ test/util.test.js
@@ -5,16 +5,21 @@ describe("Add", () => {
       expect(add(2, 2)).toBe(4);
     });
   });
-});
-describe("Failure", () => {
-  it("Should throw an error if 1st argument isn't a string", () => {
-    expect(() => add("2", 2).toThrowError());
+  describe("Failure", () => {
+    it("Should throw an error if either argument isn't a string", () => {
+      expect(() => add("2", 2).toThrowError());
+    });
   });
 });
 describe("Minus", () => {
   describe("Success", () => {
     it("Should return the sum of the 2 passed arguments", () => {
       expect(minus(6, 3)).toBe(3);
+    });
+  });
+  describe("Failure", () => {
+    it("Should throw an error if either argument isn't a string", () => {
+      expect(() => minus("6", 3).toThrowError());
     });
   });
 });
